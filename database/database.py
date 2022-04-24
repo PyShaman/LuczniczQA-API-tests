b68de0caae1b6dd1e78861b4df75fad874bcff0f
Michal Bek <mib@emakina.com>
2022-04-24 07:16:25 +0200

    fix

---
+++ database/database.py
@@ -0,0 +1,53 @@
+import motor.motor_asyncio
+from bson import ObjectId
+from decouple import config
+
+from .database_helper import student_helper, admin_helper
+
+MONGO_DETAILS = config('MONGO_DETAILS')
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+
+database = client.students
+
+student_collection = database.get_collection('students_collection')
+admin_collection = database.get_collection('admins')
+
+
+async def add_admin(admin_data: dict) -> dict:
+    admin = await admin_collection.insert_one(admin_data)
+    new_admin = await admin_collection.find_one({"_id": admin.inserted_id})
+    return admin_helper(new_admin)
+
+
+async def retrieve_students():
+    students = []
+    async for student in student_collection.find():
+        students.append(student_helper(student))
+    return students
+
+
+async def add_student(student_data: dict) -> dict:
+    student = await student_collection.insert_one(student_data)
+    new_student = await student_collection.find_one({"_id": student.inserted_id})
+    return student_helper(new_student)
+
+
+async def retrieve_student(id: str) -> dict:
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        return student_helper(student)
+
+
+async def delete_student(id: str):
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        await student_collection.delete_one({"_id": ObjectId(id)})
+        return True
+
+
+async def update_student_data(id: str, data: dict):
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        student_collection.update_one({"_id": ObjectId(id)}, {"$set": data})
+        return True
---
+++ routes/student.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+
+from database.database import *
+from models.student import *
+
+router = APIRouter()
+
+
+@router.get("/", response_description="Students retrieved")
+async def get_students():
+    students = await retrieve_students()
+    return response_model(students, "Students data retrieved successfully") \
+        if len(students) > 0 \
+        else response_model(
+        students, "Empty list returned")
+
+
+@router.get("/{id}", response_description="Student data retrieved")
+async def get_student_data(id):
+    student = await retrieve_student(id)
+    return response_model(student, "Student data retrieved successfully") \
+        if student \
+        else error_response_model("An error occured.", 404, "Student doesn't exist.")
+
+
+@router.post("/", response_description="Student data added into the database")
+async def add_student_data(student: StudentModel = Body(...)):
+    student = jsonable_encoder(student)
+    new_student = await add_student(student)
+    return response_model(new_student, "Student added successfully.")
+
+
+@router.delete("/{id}", response_description="Student data deleted from the database")
+async def delete_student_data(id: str):
+    deleted_student = await delete_student(id)
+    return response_model("Student with ID: {} removed".format(id), "Student deleted successfully") \
+        if deleted_student \
+        else error_response_model("An error occured", 404, "Student with id {0} doesn't exist".format(id))
+
+
+@router.put("{id}")
+async def update_student(id: str, req: UpdateStudentModel = Body(...)):
+    updated_student = await update_student_data(id, req.dict())
+    return response_model("Student with ID: {} name update is successful".format(id),
+                          "Student name updated successfully") \
+        if updated_student \
+        else error_response_model("An error occurred", 404, "There was an error updating the student.".format(id))
