59a88883e3ec41239bf34545c4ec42e13f2cee2c
Michał Bek <michal.bek@gmail.com>
2022-04-27 22:21:07 +0200

    adding uuid4 to response headers (#6)
    Co-authored-by: Michal Bek <mib@emakina.com>

--- routes/admin.py
+++ routes/admin.py
@@ -1,7 +1,8 @@
-from fastapi import Body, APIRouter
+from fastapi import Body, APIRouter, Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import HTTPBasicCredentials
 from passlib.context import CryptContext
+from uuid import uuid4
 
 from database.database import admin_collection
 from auth.jwt_handler import sign_jwt
@@ -14,7 +15,8 @@ hash_helper = CryptContext(schemes=["bcrypt"])
 
 
 @router.post("/login")
-async def admin_login(admin_credentials: HTTPBasicCredentials = Body(...)):
+async def admin_login(response: Response, admin_credentials: HTTPBasicCredentials = Body(...)):
+    response.headers["X-Lucznicz-QAt"] = str(uuid4())
     admin_user = await admin_collection.find_one({"email": admin_credentials.username}, {"_id": 0})
     if admin_user:
         password = hash_helper.verify(
@@ -28,7 +30,8 @@ async def admin_login(admin_credentials: HTTPBasicCredentials = Body(...)):
 
 
 @router.post("/")
-async def admin_signup(admin: AdminModel = Body(...)):
+async def admin_signup(response: Response, admin: AdminModel = Body(...)):
+    response.headers["X-Lucznicz-QAt"] = str(uuid4())
     admin_exists = await admin_collection.find_one({"email": admin.email}, {"_id": 0})
     if admin_exists:
         return "Email already exists"
--- routes/status.py
+++ routes/status.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Body
+from uuid import uuid4
+
+from fastapi import APIRouter, Body, Response
 from fastapi.encoders import jsonable_encoder
 
 from database.database import *
@@ -8,14 +10,16 @@ router = APIRouter()
 
 
 @router.post("/", response_description="Status data added into the database")
-async def add_status_data(status: ApplicationStatus = Body(...)):
+async def add_status_data(response: Response, status: ApplicationStatus = Body(...)):
+    response.headers["X-Lucznicz-QAt"] = str(uuid4())
     app_status = jsonable_encoder(status)
     new_status = await add_status(app_status)
     return response_model(new_status, "Status added successfully.")
 
 
 @router.get("/", response_description="Status retrieved")
-async def get_status():
+async def get_status(response: Response):
+    response.headers["X-Lucznicz-QAt"] = str(uuid4())
     status = await retrieve_status()
     return response_model(status, "Status data retrieved successfully") \
         if len(status) > 0 \
